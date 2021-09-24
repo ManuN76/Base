5a029d389578387b91c4bbe71675a9a1663b6252
ManuN76 <manu.n2@free.fr>
2021-09-24 08:06:54 +0200

    driver with convert

--- driver.js
+++ driver.js
@@ -1,10 +1,22 @@
-window.addEventListener("message", async function(event) {
-  const { origin, data: { key, params } } = event;
+function convert(x) {
+  if (x instanceof Date) {
+    return x.toISOString();
+  } else if (Array.isArray(x)) {
+    return x.map(convert);
+  } else {
+    return x;
+  }
+}
+
+window.addEventListener("message", async function (event) {
+  const {
+    origin,
+    data: { key, params },
+  } = event;
 
   let result;
   let error;
   try {
- 
     result = await window.function(...params);
   } catch (e) {
     result = undefined;
@@ -18,6 +30,7 @@ window.addEventListener("message", async function(event) {
   const response = { key };
   if (result !== undefined) {
     // FIXME: Remove `type` once that's in staging
+    result = convert(result);
     response.result = { type: "string", value: result };
   }
   if (error !== undefined) {
